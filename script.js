4ec38170df9ec147c61a7e4dab36a5c6b612bcee
James Meegan <jmeegan2@owlmail.harford.edu>
2021-12-20 22:51:41 -0500

    doing some js excersices to understand it better

---
+++ script.js
@@ -0,0 +1,27 @@
+// let hello = 'hello world!';
+
+// let message;
+
+// //copy 'hello world' from hello into message
+// message = hello;
+
+// //now two variables hold the same data
+// alert(hello); // Hello world!
+// alert(message); //Hello world!
+
+// let $ = 1; // declared a variable with the name "$"
+// let _ = 2; // and now a variable with the name "_"
+
+// alert($ + _); // 3
+
+// const myBirthday = '22.04.2000';
+
+// alert(myBirthday);
+
+// let admin ;
+// let nameIs;
+
+// nameIs = 'John';
+// admin = nameIs;
+
+// alert(admin);
